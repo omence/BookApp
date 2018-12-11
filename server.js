e4acbed37fc9d3e583a2c01fbbe41a1045253375
jason few <omence11@gmail.com>
2018-12-11 10:04:34 -0800

    feature one complete

--- server.js
+++ server.js
@@ -3,24 +3,32 @@
 const express = require('express');
 const ejs = require('ejs');
 const superagent = require('superagent');
-const app = express();
 const cors = require('cors');
 const pg = require('pg');
-const PORT = process.env.PORT || 3000;
 require('dotenv').config();
+const app = express();
+
 
 app.use(cors());
-const client = new pg.Client(process.env.DATABASE_URL);
-client.on('error', error => console.log(error));
-client.connect();
 
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+
+
+//const client = new pg.Client(process.env.DATABASE_URL);
+// client.on('error', error => console.log(error));
+// client.connect();
+
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
 app.post('/search', (request, response) => {
 
 });
 
+app.get ('/', (request, response) => {
+  response.render('../views/pages/index');
+});
 
 
 
