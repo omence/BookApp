95cc6656bc9c5ca313f015242781516fca61f4e5
Clarice <claricostello@gmail.com>
2018-12-11 12:09:48 -0800

    wip

--- server.js
+++ server.js
@@ -18,18 +18,52 @@ app.set('view engine', 'ejs');
 //const client = new pg.Client(process.env.DATABASE_URL);
 // client.on('error', error => console.log(error));
 // client.connect();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 
-app.post('/search', (request, response) => {
-  console.log('my request: ', request.body);
-  response.sendfile('../views/pages/seaches/show.ejs', {root: './public'});
+
+
+app.get('../views/pages/searches/show', getBooks);
+
+function getBooks(request, response){
+  const bookHandler = {
+    query: request.query.data,
+  };
+  getBooks.lookup(bookHandler);
+}
+
+function Books(query, data) {
+  this.search_query = query;
+  this.author = data.volumeInfo.authors;
+  this.title = data.volumeInfo.title;
+  this.selfLink = data.selfLink;
+}
+
+Books.fetchBooks = query => {
+  const _URL = `https://www.googleapis.com/books/v1/volumes?q=${query}:inauthor${query}`;
+  const values = [handler.query];
+
+  return superagent.get(_URL).then(data => {
+    console.log('got data from API');
+    const allBooks = data.body.results.map( thing => {
+      const books = new Books();
+    console.log('books', books);
+    return books;
+    });
+    return allBooks;
+  });
+};
+
+app.get('/', (req, res) => {
+  res.render('../views/pages/index');
 });
 
-app.get ('/', (request, response) => {
-  response.render('../views/pages/index');
-});
+Books.lookup= handler => {
+  Books.fetchBooks(request.query.data).then(data => {
+    response.send(data);
+  });
+};
+
 
 
 
